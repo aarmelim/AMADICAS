2f6c27fb0f6dc1a2f7f6d2ce2f5428b2ecef5f55
Adriano Mantovani Armelim <aarmelim@hotmail.com>
2020-05-13 20:57:53 -0300

    ajustes para equalizar

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
         <div class="header-wrap">
             <div class="header-top d-flex justify-content-between align-items-center">
                 <div class="logo">
-                	<a href=".." id=titulo_pagina>AMADICAS</a>
+                	<a href=".." id=titulo_pagina><img src="_img/Logo22.png"></a>
                 </div>
             	<?php require_once("menu.php"); ?>
         	</div>
